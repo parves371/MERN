2c4522b85b6919079b809fd0042025003170f46f
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-03-18 14:39:47 +0600

    Create a basic HTTP server serving an HTML file
    - This commit adds a simple Node.js script that creates an HTTP server using the `http` module from Node.js core and serves a basic HTML file (`index.html`) to the client. The server listens on port 3000 by default, and requests to the root path (`/`) are responded with the contents of the `index.html` file. Requests to other paths return a 404 Not Found error.
    
    -  The `fs` module is used to read the HTML file from the filesystem, and the `path` module is used to ensure cross-platform compatibility when constructing file paths.
    
    - This setup provides a basic foundation for serving static content over HTTP using Node.js.

--- node js/5-http routing/index.js
+++ node js/5-http routing/index.js
@@ -1,11 +1,34 @@
 const http = require('http');
+const fs = require('fs');
+
+
 const port = 5000;
 const hostName = "127.0.0.1";
 
-
 const myServer = http.createServer((req, res) => {
-    res.write("wellcome tp my server");
-    res.end();
+    const handleReadFile = (pathName, statusCode) => {
+        fs.readFile(pathName, 'utf-8', (err, data) => {
+            res.writeHead(statusCode, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        })
+    }
+
+    if (req.url === "/") {
+        handleReadFile('./views/home.html', 200);
+    }
+    else if (req.url === "/about") {
+        handleReadFile('./views/about.html', 200);
+    }
+    else if (req.url === "/contact") {
+        handleReadFile('./views/contact.html', 200);
+    }
+    else if (req.url === "/shope") {
+        handleReadFile('./views/shope.html', 200);
+    }
+    else {
+        handleReadFile('./views/404.html', 404);
+    }
 });
 
 myServer.listen(port, hostName, () => {
