a46d37f4373152553bc0d780a249f7dffa6a6b6e
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-03-29 16:01:42 +0600

    Implement schema models and POST/GET methods

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -3,6 +3,21 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const productsSchema = new mongoose.Schema({
+    title: String,
+    price: Number,
+    description: String,
+    createAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const Product = mongoose.model('Products', productsSchema);
+
 const connectDb = async () => {
     try {
         await mongoose.connect('mongodb://localhost:27017/e-comm');
@@ -14,9 +29,52 @@ const connectDb = async () => {
 };
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, async () => {
-    console.log(`Example app listening on port ${port}!`)
-    await connectDb()
 
+
+// create data
+app.post("/products", async (req, res) => {
+    try {
+        const { title, price, description } = req.body;
+        const newProduct = new Product({
+            title: title,
+            price: price,
+            description: description,
+        })
+        const ProductData = await newProduct.save();
+        res.status(201).send(ProductData);
+
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        });
+    }
+})
+// read data
+
+app.get("/products/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const productData = await Product.findOne({ _id: id }, { title: 0, price: 1, description: 1, _id: 0 });
+        if (productData) {
+            res.status(200).send(productData);
+        } else {
+            res.status(404).send({
+                message: "Data not found"
+            })
+        }
+
+    } catch (error) {
+        res.status(500).send({
+            message: error.message
+        });
+    }
+})
+
+
+
+
+app.listen(port, async () => {
+    console.log(`Example app listening on port ${port}`)
+    await connectDb()
 }
 );
